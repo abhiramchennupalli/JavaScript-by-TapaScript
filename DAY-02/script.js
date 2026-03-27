2fbd486a210b74220346d98bd3216dc695fbc5ca
abhiram chennupalli <abhiramchennupalli@gmail.com>
2026-03-27 16:17:42 +0530

    Update DAY-02 script

---
+++ DAY-02/script.js
@@ -0,0 +1,20 @@
+let name = "Abhiram";
+let age = 23;
+let isStudent = true;
+console.log(name);
+
+const student1 = {
+    name: 'Ruchitha',
+    age: 21,
+    address: "Ponnur",
+};
+
+
+console.log(student1.age);
+console.log(student1.name);
+let fruit = 'banana';
+let vegitable = 'carrot';
+fruit = vegitable;
+console.log(fruit);
+
+student1.age = 11;
