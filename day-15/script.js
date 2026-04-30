63a2191b3b57bde225ea1f65dcc791a52d8c0197
abhiram chennupalli <abhiramchennupalli@gmail.com>
2026-04-30 17:57:47 +0530

    learnrd filter and map methods

--- day-15/script.js
+++ day-15/script.js
@@ -409,6 +409,100 @@ console.log("Hello world this is array revision");
     function con() {
         console.log("Button Clicked!");
 
-        
+
     }
+}
+
+{
+    const numbers = [1, 2, 3, 4, 5, 6];
+    const evenNumber = numbers.filter(function (number) {
+        return number % 2 === 0;
+    });
+    console.log(evenNumber);
+
+}
+
+{
+    let customers = [
+        {
+            id: 1,
+            f_name: "Abby",
+            l_name: "Thomas",
+            gender: "M",
+            married: true,
+            age: 32,
+            expense: 500,
+            purchased: ["Shampoo", "Toys", "Book"],
+        },
+
+
+        {
+            id: 2,
+            f_name: "Jerry",
+            l_name: "Tom",
+            gender: "M",
+            married: true,
+            age: 64,
+            expense: 100,
+            purchased: ["Stick", "Blade"],
+        },
+
+        {
+            id: 3,
+            f_name: "Dianna",
+            l_name: "Cherry",
+            gender: "F",
+            married: true,
+            age: 22,
+            expense: 1500,
+            purchased: ["Lipstik", "Nail Polish", "Bag", "Book"],
+        },
+
+        {
+            id: 4,
+            f_name: "Dev",
+            l_name: "Currian",
+            gender: "M",
+            married: true,
+            age: 82,
+            expense: 90,
+            purchased: ["Book"],
+        },
+
+        {
+            id: 5,
+            f_name: "Maria",
+            l_name: "Gomes",
+            gender: "F",
+            married: false,
+            age: 7,
+            expense: 300,
+            purchased: ["Toys"],
+        },
+    ];
+
+    const seniorCitizen = customers.filter(function (people) {
+        return people.age >= 60;
+    });
+    console.log(seniorCitizen);
+
+    const boughtBook = customers.filter(function (people) {
+        return people.purchased.includes("Toys");
+    });
+
+    console.log(boughtBook);
+    const newExpence = customers.map(function (expense) {
+        return {
+            id: expense.id,
+            f_name: expense.f_name,
+            l_name: expense.l_name,
+            gender: expense.gender,
+            age: expense.age,
+            expense: expense.expense + expense.expense * 0.18,
+            purchased: expense.purchased,
+        }
+    });
+    console.log(newExpence);
+    
+
 }
