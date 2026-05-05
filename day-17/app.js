56774cfb8c0440ed3befb4ffc06a9e67d8a43bd2
abhiram chennupalli <abhiramchennupalli@gmail.com>
2026-05-05 18:02:48 +0530

    started dom manipulation

---
+++ day-17/app.js
@@ -0,0 +1,8 @@
+{
+    let para = document.getElementById('paraId');
+    console.log(para);
+    function highlight() {
+        para.style.backgroundColor = 'yellow';
+        console.log("Button Clicked")
+    }
+}
