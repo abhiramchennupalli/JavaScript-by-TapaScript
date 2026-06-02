068a1f1e00cb1884cbb363ccb2d07f65602ae1a7
abhiram chennupalli <abhiramchennupalli@gmail.com>
2026-06-02 17:55:11 +0530

    added

--- day-19/app.js
+++ day-19/app.js
@@ -1,10 +1,12 @@
-console.log('Hello World');
-const btn = document.querySelector(".click-me-button");
-let count = 0;
-btn.addEventListener("click", () => {
-    console.log(count)
-    count++;
-});
+// const { createElement } = require("react");
+
+// console.log('Hello World');
+// const btn = document.querySelector(".click-me-button");
+// let count = 0;
+// btn.addEventListener("click", () => {
+//     console.log(count)
+//     count++;
+// });
 
 // {
 //     const bubblingBtn = document.querySelector('.child');
@@ -59,4 +61,115 @@ btn.addEventListener("click", () => {
 
         console.log('parent clicked');
     })
+}
+{
+
+    const h1Elem = document.querySelector(".class-list");
+
+    h1Elem.addEventListener("click", (e) => {
+        h1Elem.classList.toggle("class-list-example");
+        console.log(e.target);
+        console.log(e.currentTarget);
+    });
+
+
+}
+
+{
+    const numbers = [2, 3, 4, 5, 6];
+    const doubledNumbers = numbers.map((number) => {
+        return number * 2;
+    }).join();
+
+    console.log(doubledNumbers);
+}
+{
+
+    const numbers = [1, 2, 3, 4, 5];
+    function addFive(number) {
+        return number = number + 5;
+    }
+
+    console.log(numbers.map(addFive))
+}
+{
+    const names = ["abhiram", "ganesh", "ruchitha"];
+
+    const upperCaseArray = names.map((name) => name.toUpperCase());
+
+    console.log(upperCaseArray)
+}
+
+{
+    const services = [
+        { title: "Web Development" },
+        { title: "SEO" },
+        { title: "Testing Services" }
+    ];
+
+    const singleArray = services.map((service) => {
+        return service.title;
+    });
+
+    console.log(singleArray)
+}
+{
+
+    const services = [
+        { title: "Web Development" },
+        { title: "SEO" },
+        { title: "Testing Services" }
+    ];
+    const singleArray = services.map((service) => {
+        return `<p>${service.title}</p>`;
+
+    }).join('');
+    console.log(singleArray)
+
+}
+{
+    const services = [
+        {
+            title: "Web Development",
+            image: "web.svg"
+        },
+        {
+            title: "SEO",
+            image: "seo.svg"
+        }
+    ];
+
+    document.querySelector('.mapFucnctionTesting').innerHTML =
+        services.map((service) => {
+            return `<div>
+                        <img src=${service.image}>
+                        <h3>${service.title}</h3>
+                    </div>`
+        }).join('');
+}
+
+{
+    const names = ["Abhiram", "Ganesh", "Ruchitha"];
+
+    const newArray = names.map((name) => `hello ${name}`);
+    console.log(newArray)
+}
+
+{
+    const products = [
+        { name: "Phone", price: 20000 },
+        { name: "Laptop", price: 50000 },
+        { name: "Watch", price: 3000 }
+    ];
+
+    console.log(products.map((product) => product.price))
+}
+
+{
+    const names = ["Abhiram", "Ganesh", "Ruchitha"];
+
+    console.log(names.map((name, index) => {
+        return `${index + 1}. ${name}`;
+    }))
+
 }
